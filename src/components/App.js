beffffdae375cbb21d2fc58e36e56388d54d6b0c
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2019-08-31 18:06:28 -0500

    change dir structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import GameBoard from "./GameBoard";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="ui  container">
+          <GameBoard />
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
